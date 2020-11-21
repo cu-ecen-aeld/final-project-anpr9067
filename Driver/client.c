dc0e6ec53b2b8cfcb7dcb860a11049bc3ed4858c
Antara Prakash <anpr9067@colorado.edu>
2020-11-20 21:48:07 -0700

    changes in client

--- Driver/client.c
+++ Driver/client.c
@@ -1,9 +1,11 @@
 // Client side C/C++ program to demonstrate Socket programming 
 #include <stdio.h> 
+#include <stdlib.h>
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <unistd.h> 
 #include <string.h> 
+#include <syslog.h>
 #define PORT 9000 
 
 int main(int argc, char const *argv[]) 
@@ -23,7 +25,7 @@ int main(int argc, char const *argv[])
 			exit(1);
 		}
 	}
-	
+
 	int sock = 0, valread; 
 	struct sockaddr_in serv_addr; 
 	char *hello = "Hello from client\n"; 
